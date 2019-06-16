d6d97a5380cde6ba9bef4515648c344c719c4641
Junyoung, Sung <junyoung.sung@navercorp.com>
2019-06-16 21:40:14 +0900

    Fix documentation bug

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ echo "Overwriting old dependency requirements..."
 cat >./ceph/admin/doc-requirements.txt <<EOF
 Sphinx == 1.8.3
 git+https://github.com/ceph/sphinx-ditaa.git@py3#egg=sphinx-ditaa
-git+https://github.com/michaeljones/breathe#egg=breathe
+# newer versions of breathe will require Sphinx >= 2.0.0 and are Python3 only
+breathe==4.12.0
 # 4.2 is not yet release at the time of writing, to address CVE-2017-18342,
 # we have to use its beta release.
-pyyaml>=4.2b1
-cython
+pyyaml>=4.2b1<Paste>
 EOF
 
 rm -rf ceph/doc-orig
